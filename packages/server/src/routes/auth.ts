3276204221cc9a6e147bf163f6f2ce6cff7f422f
mlnvgl <vogel.melanie@outlook.com>
2026-03-13 21:36:01 +0100

    Fresh startover.

--- packages/server/src/routes/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { login, me } from "../controllers/auth.js";
-
-export const authRoutes = Router();
-
-authRoutes.post("/login", login);
-authRoutes.get("/me", me);
